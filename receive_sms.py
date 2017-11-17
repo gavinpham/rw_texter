6e3343d1ab92aadec1207eddaca4b0bc608b1ca5
gavinpham <gpham@smu.edu>
2017-11-17 13:38:08 -0600

    Added base functionality to receive and reply to hardcoded messages

--- receive_sms.py
+++ receive_sms.py
@@ -1,4 +1,4 @@
-import os, constants
+import os, constants, editdistance
 from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse
 
@@ -10,11 +10,14 @@ def sms_reply():
 	#Retreive body text sent from user
 	body = str(request.form['Body']).lower()
 
+	print body
+	editDistanceLipitor = editdistance.eval(body, constants.LIPITOR_EXAMPLE)
+	print editDistanceLipitor
 
 	resp=MessagingResponse()
 
 	
-	if body == "lipitor, 40 mg, 30 tablets":
+	if editDistanceLipitor <= 6:
 		resp.message(constants.RESPONSE_1)	#Hardcoded response to 'Lipitor, 40 mg, 30 tablets'
 	elif body == "yes":
 		resp.message(constants.RESPONSE_2)	#Hardcoded response to 'YES'
