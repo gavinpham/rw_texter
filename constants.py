96d9f7aecbbff7a58d14f8e45c1cabea2f49a8e9
gavinpham <gpham@smu.edu>
2017-11-18 11:37:44 -0600

    Added handling for individually sent messages of name, dosage, and quantity.

--- constants.py
+++ constants.py
@@ -1,5 +1,18 @@
+#NUMBERS
 DELAY_IN_SECONDS = 10
-LIPITOR_EXAMPLE = "lipitor, 40 mg, 30 tablets"
-RESPONSE_1 = "Thank you for texting, we'll get back to you shortly! \n\nPlease allow up to 3 minutes for a response. \n\nWhile you are waiting, have you asked your doctor questions about your medication such as \"How does my medicine work?\"\n\nIf you would like a list of more questions to ask your doctor, please reply YES."
-RESPONSE_2 = "What are the side effects of this medication?\n\nHow long until the medication starts to take effect?\n\nWill this medication interfere with anything?\n\nHow should I take this medicaiton?"
+LENIENT_EDIT_DISTANCE = 6
+STRICT_EDIT_DISTANCE = 1
+
+#STRINGS
+PRESCRIPTION_FULL= "lipitor, 40 mg, 30 tablets"
+PRESCRIPTION_FULL_LONG= "lipitor, 40 milligrams, 30 tablets"
+PRESCRIPTION_NAME= "lipitor"
+PRESCRIPTION_DOSAGE= "40 mg"
+PRESCRIPTION_DOSAGE_LONG ="40 milligrams"
+PRESCRIPTION_QUANTITY= "30 tabs"
+PRESCRIPTION_QUANTITY_LONG= "30 tablets"
+DOSAGE_ASK = "What is the dosage of your prescription?\n(Example: 40 milligrams)\n\nA dosage is the amount of medication in one tablet of your prescription.\n\nIf you are not sure how to find this number, ask your doctor for assistance."
+QUANTITY_ASK = "What is the quantity of tablets that you have been prescribed for purchasing?\n(Example: 30 tablets)\n\nThe quantity for your prescription is the number of pills or tablets that come in one container of your medicaiton.\n\nIf you are not sure how to find this number, ask your doctor for assistance."
+THANK_YOU_RESPONSE = "Thank you for texting a prescription, we'll get back to you shortly! \n\nPlease allow up to 3 minutes for a response. \n\nWhile you are waiting, have you asked your doctor questions about your medication such as \"How does my medicine work?\"\n\nIf you would like a list of more questions to ask your doctor, please reply YES."
+QUESTION_RESPONSE = "What are the side effects of this medication?\n\nHow long until the medication starts to take effect?\n\nWill this medication interfere with anything?\n\nHow should I take this medicaiton?"
 PRICE_RESPONSE = "The average cost of Lipitor per pharmacy is:\nCostco: $410\nWalmart: $411\nWalgreens: $551\n\nThe generic for Lipitor is Atorvastatin and the average cost per pharmacy is:\nWalmart: $4\nCostco: $8\nWalgreens: $42\n\nIf you're still worried about these prices, ask your doctor about alternatives."
